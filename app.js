5f818eaa590ca1b438b3ed25bd6ff4669b845d75
Kikibyk <57362643+Kikibyk@users.noreply.github.com>
2021-01-27 15:02:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const { VK } = require('vk-io'); // Подключаем библиотеку
-const vk = new VK({ token: "TOKEN" }); // Авторизуемся
+const vk = new VK({ token: "TOKEN" }); // Авторизуемся по токену
 const commands = []; // Массив с командами
 
-// Добавление команд из папки commands в массив 
-require('fs').readdir("./commands/", function(err, files){
+// Добавление команд из папки commands в массив
+require('fs').readdir("./commands/", (err, files) => {
     files.forEach(f => commands.push(require(`./commands/${f}`)));
 });
 
 // Событие нового сообщения
 vk.updates.on("message_new", async (message) => {
-    if(!message.text) return;
-
-    message.args = message.text.split(" ");
-    commands.forEach(async (command) => {
-        if (message.text.toLowerCase().indexOf(command.info.name.toLowerCase()) == 0) await command.execute(message);
-    });
-}).start();
+    const command = commands.find(cmd => cmd.info.name.test(message.text)); // Поиск команды
+    if(!command) return; // Завершаем функцию, если команда не найдена
+    
+    message.args = message.text.match(command.info.name); // Получаем аргументы команды
+    command.execute(message); // Выполняем код команды
+}).start(); // Включаем обработку события
